3fbb1c7a40caaf4bfb045df54c37fed46ad78d35
Alek07 <alekissac@gmail.com>
2022-06-03 17:18:50 -0500

    se quito los Grids que estaban de mas

--- src/Pages/Main/Home.jsx
+++ src/Pages/Main/Home.jsx
@@ -6,48 +6,32 @@ import "./Home.css";
 // import Grid from '@mui/material/Grid';
 
 export default function Home() {
-	const image = require("../../Assets/Images/home.png");
-	return (
-		<Grid
-			container
-			direction="row"
-			justify="center"
-			alignItems="center"
-			alignContent="center"
-			wrap="nowrap"
-      width="100%"
-		>
-			<Grid
-				direction="row"
-				justifyContent="center"
-				alignItems="center"
-				spacing={2}
-				height="100vh"
-			>
-				<Grid maxWidth="sm">
-					<Typography variant="h2" color="initial" align="center">
-						Aprender a Programar como un Pro
-					</Typography>
-					<Typography variant="body1" color="initial" align="justify">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
-						laborum impedit sequi iure error reprehenderit nam praesentium
-						explicabo velit dolor, temporibus recusandae nisi mollitia tempore?
-						Rem, illo! Sequi, dolorem tempore.
-					</Typography>
-					<Stack
-						direction="row"
-						justifyContent="center"
-						alignItems="center"
-						spacing={10}
-					>
-						<Button variant="contained">Iniciar Sesion</Button>
-						<Button variant="contained">Registrarse</Button>
-					</Stack>
-				</Grid>
-				<Grid maxWidth="sm">
-					<img src={image} alt="Home-logo" />
-				</Grid>
-			</Grid>
-		</Grid>
-	);
+  const image = require("../../Assets/Images/home.png");
+  return (
+    <Grid container>
+      <Grid item md={6}>
+        <Typography variant="h2" color="initial" align="center">
+          Aprender a Programar como un Pro
+        </Typography>
+        <Typography variant="body1" color="initial" align="justify">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+          laborum impedit sequi iure error reprehenderit nam praesentium
+          explicabo velit dolor, temporibus recusandae nisi mollitia tempore?
+          Rem, illo! Sequi, dolorem tempore.
+        </Typography>
+        <Stack
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          spacing={10}
+        >
+          <Button variant="contained">Iniciar Sesion</Button>
+          <Button variant="contained">Registrarse</Button>
+        </Stack>
+      </Grid>
+      <Grid item md={6}>
+        <img src={image} alt="Home-logo" />
+      </Grid>
+    </Grid>
+  );
 }
