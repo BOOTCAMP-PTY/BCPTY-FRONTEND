1be4de9269d21a9f73515824b7bf277a2d0ce828
Alek07 <alekissac@gmail.com>
2022-06-06 11:56:33 -0500

    theme setup and project sturcture cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import Home from "./Pages/Main/Home";
-import "./App.css";
-import Login from "./Pages/Login/Login";
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export default function App() {
-	return (
-		// <main>
-		<Router>
-			<Routes>
-				<Route path="/" element={<Home />}/>
-				<Route path="/login" element={<Login />}/>
-			</Routes>
-		</Router>
-		// </main>
-	);
+  return (
+    // <main>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+    // </main>
+  );
 }
