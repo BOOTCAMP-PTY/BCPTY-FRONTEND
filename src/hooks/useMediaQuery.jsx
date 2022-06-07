1ef10358d2749d292f6dae41ef6681798e5d3b66
Alek07 <alekissac@gmail.com>
2022-06-06 20:48:05 -0500

    cambio en el valor que se retorna

--- src/hooks/useMediaQuery.jsx
+++ src/hooks/useMediaQuery.jsx
@@ -3,9 +3,8 @@ import { useMediaQuery as MUIUseMediaQuery } from "@mui/material/";
 
 const useMediaQuery = (breakpoint) => {
   const theme = useTheme();
-  const matches = MUIUseMediaQuery(theme.breakpoints.up(breakpoint));
+  return MUIUseMediaQuery(theme.breakpoints.up(breakpoint));
 
-  return matches;
 }
 
 export default useMediaQuery
