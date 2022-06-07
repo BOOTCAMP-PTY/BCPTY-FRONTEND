677268333a55e89659c87abdf35f07ef6190d3b2
captainsparrow10 <javier1009rm@gmail.com>
2022-06-07 06:18:28 -0500

    Iniciando el login de la rama theme

--- src/components/headers/HeaderPage.jsx
+++ src/components/headers/HeaderPage.jsx
@@ -5,7 +5,7 @@ import CodeIcon from '@mui/icons-material/Code';
 export default function HeaderPage() {
 	return (
 		<Box sx={{ flexGrow: 1,width: 1,height: '10%', flexWrap: 'wrap'}}>
-			<AppBar position="static">
+			<AppBar position="static"  sx={{ backgroundColor: "common.white" }}>
 				<Toolbar>
 					<CodeIcon sx={{ mr: 1 }} />
 					<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
