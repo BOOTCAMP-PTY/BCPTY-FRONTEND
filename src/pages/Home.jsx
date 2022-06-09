04288a21a260e42ec89dcbb7226013b2da49ae86
Sparrow <javier1009rm@gmail.com>
2022-06-09 09:51:17 -0500

    Agregue el responsive del headerHome y arregle la Home, Falta el responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,76 +7,113 @@ import homeBanner from "../assets/images/svg/landing_banner.svg";
 import { Link } from "react-router-dom";
 
 export default function Home() {
+	return (
+		<Container
+			maxWidth={false}
+			sx={{
+				height: "100vh",
+				padding: "0 !important",
+				backgroundColor: "common.white",
+			}}
+		>
+			<HeaderHome />
+			<Grid
+				container
+				alignItems="center"
+				sx={{
+					padding: { xs: "0 2rem", md: "2rem" },
+					marginTop: { xs: 0, md: "5rem" },
+				}}
+			>
+				<Grid
+					item
+					sm={12}
+					md={6}
+					sx={{
+						padding: { xs: 0, sm: "2rem", md: "4rem" },
+					}}
+				>
+					<Typography
+						variant="h1"
+						color="initial"
+						sx={{
+							marginBottom: { xs: "41", sm: "43px" },
+						}}
+						align="left"
+					>
+						Aprende a Programar como un Pro
+					</Typography>
+					<Typography
+						variant="body1"
+						color="initial"
+						sx={{
+							marginBottom: { xs: "64", sm: "43px" },
+						}}
+						align="justify"
+					>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+						laborum impedit sequi iure error reprehenderit nam praesentium
+						explicabo velit dolor, temporibus recusandae nisi mollitia tempore?
+						Rem, illo! Sequi, dolorem tempore.
+					</Typography>
+					<Grid
+						container
+						spacing={{ xs: 4, sm: 2 }}
+						columns={{ xs: 12, sm: 12 }}
+					>
+						<Grid item sm={6} xs={12} align="center">
+							<Button variant="contained" component={Link} sx={{
+                width : "194px",
+                height : "51px"
+              }} to="/login">
+                <Typography variant="button" color="initial" align="center" sx={{
+                width : "114px",
+                height : "21px",
+                fontSize : "18px",
+                fontWeight : "600px",
+                lineHeight : "21px",
+              }}>
 
-  return (
-    <Container
-      maxWidth={false}
-      sx={{
-        height: "100vh",
-        padding: "0 !important",
-        backgroundColor: "common.white",
-      }}
-    >
-      <HeaderHome />
-      <Grid
-        container
-        alignItems="center"
-        sx={{
-          padding: { xs: "0 2rem", md: "2rem" },
-          marginTop: { xs: 0, md: "5rem" },
-        }}
-      >
-        <Grid
-          item
-          sm={12}
-          md={6}
-          sx={{
-            padding: { xs: 0, sm: "2rem", md: "4rem" },
-          }}
-        >
-          <Typography variant="h1" color="initial" align="left">
-            Aprende a Programar como un Pro
-          </Typography>
-          <Typography variant="body1" color="initial" align="justify">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
-            laborum impedit sequi iure error reprehenderit nam praesentium
-            explicabo velit dolor, temporibus recusandae nisi mollitia tempore?
-            Rem, illo! Sequi, dolorem tempore.
-          </Typography>
-          <Grid
-            container
-            spacing={{ xs: 4, sm: 2 }}
-            columns={{ xs: 12, sm: 12 }}
-          >
-            <Grid item sm={6} xs={12} align="center">
-              <Button variant="contained" component={Link} to="/login">
-                Iniciar Sesion
-              </Button>
-            </Grid>
-            <Grid item sm={6} xs={12} align="center">
-              <Button variant="contained" component={Link} to="/register">
-                Registrarse
-              </Button>
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid item sm={12} md={6} container justifyContent="center">
-          <Image
-            source={homeBanner}
-            sx={{
-              width: {
-                xs: 300,
-                sm: 450,
-                lg: 642,
-              },
-              marginTop: { xs: "4rem", md: 0 },
-            }}
-            alt="home-banner"
-            height="auto"
-            width="100%"
-          />
-        </Grid>
-      </Grid>
-    </Container>
-  );
+								Iniciar Sesion
+                </Typography>
+							</Button>
+						</Grid>
+						<Grid item sm={6} xs={12} align="center">
+							<Button variant="contained" component={Link} sx={{
+                width : "194px",
+                height : "51px"
+              }} to="/login">
+                <Typography variant="button" color="initial" align="center" sx={{
+                width : "112px",
+                height : "21px",
+                fontSize : "18px",
+                fontWeight : "600px",
+                lineHeight : "21px"
+              }}>
+
+								Registrarte
+                </Typography>
+							</Button>
+						</Grid>
+					</Grid>
+				</Grid>
+				<Grid item sm={12} md={6} container justifyContent="center">
+					<Image
+						source={homeBanner}
+						sx={{
+							width: {
+								xs: 300,
+								sm: 450,
+								lg: 642,
+							},
+							marginTop: { xs: "4rem", md: 0 },
+						}}
+						alt="home-banner"
+						height="auto"
+						width="100%"
+					/>
+				</Grid>
+			</Grid>
+		</Container>
+	);
 }
