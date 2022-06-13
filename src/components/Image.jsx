bdd41f4e4302a504274035d957a12437bb9c8a84
Alek07 <alekissac@gmail.com>
2022-06-12 22:54:50 -0500

    prop type config

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -2,17 +2,18 @@ import PropTypes from 'prop-types';
 import { Box } from '@mui/material';
 import React from 'react';
 
-const Image = ({ source, sx = {}, alt, height, width }) => {
+const Image = ({ source, sx, alt, height, width }) => {
   return (
     <Box component="img" sx={{ ...sx }} src={source} alt={alt} height={height} width={width} />
   );
 };
 
 Image.propTypes = {
-  source: PropTypes.string,
-  sx: PropTypes.object,
-  alt: PropTypes.string,
-  height: PropTypes.string | PropTypes.number
+  source: PropTypes.node,
+  sx: PropTypes.node,
+  alt: PropTypes.node,
+  height: PropTypes.node,
+  width: PropTypes.node
 };
 
 export default Image;
