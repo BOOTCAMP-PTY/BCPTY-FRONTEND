b112b1dd9bfbe61a2970a0e021a25c2346d2438f
captainsparrow10 <javier1009rm@gmail.com>
2022-06-22 09:47:27 -0500

    model dashboard

--- src/components/HomeLogin/HomeCourse.tsx
+++ src/components/HomeLogin/HomeCourse.tsx
@@ -1,4 +1,14 @@
-import { Box, Button, Container, Grid, InputAdornment, TextField, Typography } from '@mui/material'
+import {
+  Box,
+  Button,
+  Card,
+  CardMedia,
+  Container,
+  Grid,
+  InputAdornment,
+  TextField,
+  Typography,
+} from '@mui/material'
 import React from 'react'
 import HeaderPage from '../headers/HeaderPage/HeaderPage'
 import SearchIcon from '@mui/icons-material/Search'
@@ -13,12 +23,31 @@ export default function HomeCourse() {
         backgroundColor: 'common.white',
       }}>
       <HeaderPage />
-      <Grid container sx={{ padding: 0, flexGrow: 1, width: 1, flexWrap: 'wrap' }}>
+      <Grid
+        container
+        sx={{
+          padding: 0,
+          flexGrow: 1,
+          width: 1,
+          flexWrap: 'wrap',
+        }}>
+        {/* Hola, joe */}
         <Grid item xs={12} sx={{ margin: '61px 0 77px 65px' }}>
-          <Typography variant='h1' color='#3B3B3B'>
+          {/* 209px */}
+          <Typography
+            variant='h1'
+            color='#3B3B3B'
+            sx={{
+              width: '400px',
+              height: '38px',
+              fontWeight: '700',
+              fontSize: '32px',
+              lineHeight: '38px',
+            }}>
             Hola, Joe Doe
           </Typography>
         </Grid>
+        {/* Buscador */}
         <Grid container sx={{ display: 'flex', flexDirection: 'column', marginLeft: '65px' }}>
           <Grid item xs={12}>
             <Typography
@@ -27,7 +56,7 @@ export default function HomeCourse() {
               sx={{
                 width: '374px',
                 height: '33px',
-                fontFamily: '500px',
+                fontWeight: '500px',
                 fontSize: '28px',
                 lineHeight: '33px',
               }}>
@@ -69,13 +98,166 @@ export default function HomeCourse() {
                   sx={{
                     width: '98px',
                     height: '31px',
-                    fontFamily: '600px',
+                    fontWeight: '600px',
                     fontSize: '24px',
                     lineHeight: '28px',
                   }}>
                   Buscar
                 </Typography>
               </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+        {/* Continuar Viendo */}
+        <Grid
+          container
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            marginTop: '117px',
+            marginLeft: '65px',
+          }}>
+          <Grid item xs={12}>
+            {/* 213px */}
+            <Typography
+              variant='h1'
+              color='#3B3B3B'
+              sx={{
+                width: '600px',
+                height: '33px',
+                fontWeight: '500',
+                fontSize: '28px',
+                lineHeight: '33px',
+              }}>
+              Continuar Viendo
+            </Typography>
+            <Grid item xs={12} sx={{ display: 'flex', marginTop: '60px' }}>
+              <Card sx={{ width: '236px', height: '155px', marginRight: '55px' }}>
+                <CardMedia
+                  component='img'
+                  height='140'
+                  image='/static/images/cards/contemplative-reptile.jpg'
+                  alt='green iguana'
+                />
+              </Card>
+              <Card sx={{ width: '236px', height: '155px', marginRight: '55px' }}>
+                <CardMedia
+                  component='img'
+                  height='140'
+                  image='/static/images/cards/contemplative-reptile.jpg'
+                  alt='green iguana'
+                />
+              </Card>
+              <Card sx={{ width: '236px', height: '155px', marginRight: '55px' }}>
+                <CardMedia
+                  component='img'
+                  height='140'
+                  image='/static/images/cards/contemplative-reptile.jpg'
+                  alt='green iguana'
+                />
+              </Card>
+              <Card sx={{ width: '236px', height: '155px' }}>
+                <CardMedia
+                  component='img'
+                  height='140'
+                  image='/static/images/cards/contemplative-reptile.jpg'
+                  alt='green iguana'
+                />
+              </Card>
+            </Grid>
+          </Grid>
+        </Grid>
+        {/* Nuevos cursos */}
+        <Grid
+          container
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            marginTop: '102px',
+            marginLeft: '65px',
+          }}>
+          <Grid item xs={12}>
+            {/* 194px */}
+            <Typography
+              variant='h1'
+              color='#3B3B3B'
+              sx={{
+                width: '600px',
+                height: '33px',
+                fontWeight: '500',
+                fontSize: '28px',
+                lineHeight: '33px',
+              }}>
+              Nuevos cursos
+            </Typography>
+            <Grid item xs={12} sx={{ display: 'flex', marginTop: '32px' }}>
+              <Card sx={{ width: '527px', height: '262px', marginRight: '55px' }}>
+                <CardMedia
+                  component='img'
+                  height='140'
+                  image='/static/images/cards/contemplative-reptile.jpg'
+                  alt='green iguana'
+                />
+              </Card>
+              <Card sx={{ width: '527px', height: '262px', marginRight: '55px' }}>
+                <CardMedia
+                  component='img'
+                  height='140'
+                  image='/static/images/cards/contemplative-reptile.jpg'
+                  alt='green iguana'
+                />
+              </Card>
+            </Grid>
+          </Grid>
+        </Grid>
+        {/* Los mas vistos */}
+        <Grid
+          container
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            marginTop: '104px',
+            marginLeft: '65px',
+          }}>
+          <Grid item xs={12}>
+            {/* 191px */}
+            <Typography
+              variant='h1'
+              color='#3B3B3B'
+              sx={{
+                width: '600px',
+                height: '33px',
+                fontWeight: '500',
+                fontSize: '28px',
+                lineHeight: '33px',
+              }}>
+              Los mas vistos
+            </Typography>
+            <Grid item xs={12} sx={{ display: 'flex', marginTop: '30px' }}>
+              <Card sx={{ width: '342px', height: '192px', marginRight: '40px' }}>
+                <CardMedia
+                  component='img'
+                  height='140'
+                  image='/static/images/cards/contemplative-reptile.jpg'
+                  alt='green iguana'
+                />
+              </Card>
+              <Card sx={{ width: '342px', height: '192px', marginRight: '40px' }}>
+                <CardMedia
+                  component='img'
+                  height='140'
+                  image='/static/images/cards/contemplative-reptile.jpg'
+                  alt='green iguana'
+                />
+              </Card>
+              <Card sx={{ width: '342px', height: '192px', marginRight: '40px' }}>
+                <CardMedia
+                  component='img'
+                  height='140'
+                  image='/static/images/cards/contemplative-reptile.jpg'
+                  alt='green iguana'
+                />
+              </Card>
             </Grid>
           </Grid>
         </Grid>
