d1033ed6941358aacca1bf379ce8554390c37ec2
Alek07 <alekissac@gmail.com>
2022-07-02 22:22:08 -0500

    moved back to component folder

--- src/utils/const/Image.tsx
+++
@@ -1,9 +0,0 @@
-import { Box } from '@mui/material'
-import React from 'react'
-import { ImageProps } from '../../types/image'
-
-const Image: React.FC<ImageProps> = ({ source, sx, alt, height, width }) => {
-  return <Box component='img' sx={{ ...sx }} src={source} alt={alt} height={height} width={width} />
-}
-
-export default Image
