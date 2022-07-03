b4e0aa99b5216ac78692d03009125085a06bce86
Alek07 <alekissac@gmail.com>
2022-07-02 22:25:58 -0500

    main layout added

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,23 +1,10 @@
-import React from 'react'
-import { Container, Grid } from '@mui/material'
-import AsideBar from '../components/Drawer/AsideBar'
-import HomePage from '../components/Content/HomePage'
+import DashboardContent from '../components/DashboardContent'
+import { MainLayout } from '../layouts/MainLayout'
+
 export default function DashBoard() {
   return (
-    <Container
-      maxWidth={false}
-      sx={{
-        height: '100vh',
-        padding: '0 !important',
-      }}>
-      <Grid container sx={{ display: 'flex' }}>
-        <Grid item>
-          <AsideBar />
-        </Grid>
-        <Grid item xs sx={{ ml: '128px' }}>
-          <HomePage />
-        </Grid>
-      </Grid>
-    </Container>
+    <MainLayout>
+      <DashboardContent />
+    </MainLayout>
   )
 }
