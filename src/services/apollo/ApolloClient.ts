000133b05cdda48bf241f05da607fae0c8cd5ef3
JMJHOX <josemiguelaparicio507@gmail.com>
2022-07-26 20:08:02 -0500

    added endpoint for CMS, configured .env for auth and endpoint url, and installing apollo dependencies and setting it up for register

---
+++ src/services/apollo/ApolloClient.ts
@@ -0,0 +1,22 @@
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
+
+import { setContext } from '@apollo/client/link/context'
+
+const httpLink = new HttpLink({
+  uri: process.env.REACT_APP_API_URL,
+})
+
+const authLink = setContext((_: any, { headers }: any) => {
+  const token = process.env.REACT_APP_TOKEN_AUTH
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${token}`,
+    },
+  }
+})
+
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+})
